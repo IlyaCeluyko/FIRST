5b9ac73e516c617f163dc3892502de71d1176984
Илья <ilya_celuyko@mail.ru>
2017-06-30 08:37:23 +0300

    услови

--- first/first/Program.cs
+++ first/first/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/// <summary>
+/// Создать массив строк разной длины. На основе LINQ подсчитать количество строк которые начинаются с А.
+/// </summary>
 
 namespace first
 {
